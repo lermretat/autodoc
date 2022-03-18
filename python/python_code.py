0dfc61040eece69ed2d101a05c9e7cf35672e440
lmorales <luisernesto.morales@globant.com>
2022-03-17 19:53:52 -0500

    Inicio de prueba básica

---
+++ python/python_code.py
@@ -0,0 +1,21 @@
+class Clase_autodoc:
+    """_summary_
+
+    Returns:
+        {string}: mensaje motivacional
+    """    
+    @staticmethod
+    def funcion_mantenible():
+        """
+        Args:
+            param1: This is the first param.
+            param2: This is a second param.
+
+        Returns:
+            This is a description of what is returned.
+
+        Raises:
+            KeyError: Raises an exception.
+        """        
+        return "Por tanto, soy mantenible :)"
+      
