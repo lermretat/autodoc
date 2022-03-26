3a7cbe46e6465297300594e1ec8bc9c8714fccea
JASAMZ <jainersamper@gmail.com>
2022-03-26 15:19:13 -0500

    jainersamper

---
+++ javascript/jainersamper/codigo/index.js
@@ -0,0 +1,12 @@
+
+/**
+ *Este es mi nombre completo
+ * @type {string}
+ */
+ const Nombre = "Jainer Samper"
+
+ /**
+  *Esta es la materia
+  * @type {string}
+  */
+ const Materia = "Mantenimiento y evolución producto software"
