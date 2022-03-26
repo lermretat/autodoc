8b6d9ad2374d07f6f8cdb14192147f014e4d65b9
Yussed Sandoval <randy.sandoval@mercadolibre.com.co>
2022-03-26 14:20:43 -0500

    Add java doc

---
+++ java/Yussed/autodoc/src/main/java/main.java
@@ -0,0 +1,10 @@
+import method.Method;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Are yo ready? Okey, let's go!");
+        Method method = new Method();
+        method.greeting("Yussed");
+    }
+}
---
+++ java/Yussed/autodoc/src/main/java/method/Method.java
@@ -0,0 +1,13 @@
+package method;
+
+public class Method {
+
+    /**
+     * This method sends a personalized greeting with the name
+     *
+     * @param  name the name of the recipient of the greeting
+     */
+    public void greeting(String name) {
+        System.out.println("Hellow world, " + name);
+    }
+}
